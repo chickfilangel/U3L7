14a111c712f417b5f5fe94501695ad8c381eebf7
chickfilangel <43784681+chickfilangel@users.noreply.github.com>
2018-12-10 11:27:42 -0500

    Add files via upload

---
+++ lesson7/problem2.py
@@ -0,0 +1,8 @@
+x = input('Pick a number: ')
+x= int(x)
+y = input('Pick another number: ')
+y= int(y)
+
+product = str(x * y)
+
+print(product)
---
+++ lesson7/problem3.py
@@ -0,0 +1,21 @@
+print('_' * 20)
+print('100 Birthday Program')
+
+print( )
+
+#Description
+
+print('This program asks you for your current age and tells you the year that you will turn 100. ')
+
+print( )
+
+age = input('What is your age today? ')
+age = int(age)
+
+yearsleft = 100 - age
+
+total = age + str('2018')
+
+answer = 2018 + yearsleft
+
+print('_' * 65)
